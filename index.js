f4cae46ea109d6b5e83b7b68f68871639fccfc3b
Pili <mariapilar.ruizcolas@neuf.fr>
2022-04-05 15:57:05 +0200

    Post and Put Path Added

--- index.js
+++ index.js
@@ -39,6 +39,24 @@ app.get('/api/books', (req, res) => {
       }
     );
   });
+  app.put('/api/books/:bookId', (req, res) => {
+    const { bookId } = req.params;
+    const bookPropsToUpdate = req.body;
+    // On envoie une requête UPDATE à notre BdD dans la donnée qui a l’id qui est marqué en params 	
+    //substitue les données actuels pour celles que je te donne dans le body
+    connection.query(
+      'UPDATE books SET ? WHERE id = ?',
+      [bookPropsToUpdate, bookId],
+      (err) => {
+         if (err) {
+          console.log(err);
+          res.status(500).send('Error updating a book');
+        } else {
+          res.status(200).send('Book updated successfully 🎉');
+        }
+      }
+    );
+  });
 
   //TABLE USERS
   app.get('/api/users', (req, res) => {
@@ -65,6 +83,25 @@ app.get('/api/books', (req, res) => {
       }
     );
   });
+  
+  app.put('/api/users/:userId', (req, res) => {
+    const { userId } = req.params;
+    const userPropsToUpdate = req.body;
+    // On envoie une requête UPDATE à notre BdD dans la donnée qui a l’id qui est marqué en params 	
+    //substitue les données actuels pour celles que je te donne dans le body
+    connection.query(
+      'UPDATE users SET ? WHERE id = ?',
+      [userPropsToUpdate, userId],
+      (err) => {
+         if (err) {
+          console.log(err);
+          res.status(500).send('Error updating a user');
+        } else {
+          res.status(200).send('User updated successfully 🎉');
+        }
+      }
+    );
+  });
 //TABLE   BORROWING
 app.get('/api/borrowing', (req, res) => {
   connection.query('SELECT * FROM borrowing', (err, result) => {
@@ -75,6 +112,20 @@ app.get('/api/borrowing', (req, res) => {
       res.status(200).json(result);
     }
   });
+});
+app.post('/api/borrowing', (req, res) => {
+  const { users_id, books_id, deadline_date} = req.body;
+  connection.query(
+    'INSERT INTO borrowing ( users_id, books_id, deadline_date) VALUES (?, ?, ?)',
+    [ users_id, books_id, deadline_date],
+    (err, result) => {
+      if (err) {
+        res.status(500).send('Error saving the borrowing');
+      } else {
+        res.status(200).send('Borrowing successfully saved');
+      }
+    }
+  );
 });
 
 app.listen (port, (err=>{
