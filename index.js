c5671771415f116407b0fa0a820ed86464b30364
Pili <mariapilar.ruizcolas@neuf.fr>
2022-04-05 12:10:29 +0200

    first path

--- index.js
+++ index.js
@@ -13,9 +13,69 @@ connection.connect((err) => {
 });
 app.use(cors());
 app.use(express.json());
+//TABLE BOOKS
 
+app.get('/api/books', (req, res) => {
+    connection.query('SELECT * FROM books', (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error retrieving data from database');
+      } else {
+        res.status(200).json(result);
+      }
+    });
+  });
+  app.post('/api/books', (req, res) => {
+    const { title, author, is_free} = req.body;
+    connection.query(
+      'INSERT INTO books (title, author, is_free) VALUES (?, ?, ?)',
+      [title, author, is_free],
+      (err, result) => {
+        if (err) {
+          res.status(500).send('Error saving the book');
+        } else {
+          res.status(200).send('Book successfully saved');
+        }
+      }
+    );
+  });
 
-
+  //TABLE USERS
+  app.get('/api/users', (req, res) => {
+    connection.query('SELECT * FROM users', (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error retrieving data from database');
+      } else {
+        res.status(200).json(result);
+      }
+    });
+  });
+  app.post('/api/users', (req, res) => {
+    const { firstname, lastname, birthday, address, city, package} = req.body;
+    connection.query(
+      'INSERT INTO users (firstname, lastname, birthday, address, city, package) VALUES (?, ?, ?, ?,?,?)',
+      [firstname, lastname, birthday, address, city, package],
+      (err, result) => {
+        if (err) {
+          res.status(500).send('Error saving the users');
+        } else {
+          res.status(200).send('Users successfully saved');
+        }
+      }
+    );
+  });
+//TABLE   BORROWING
+app.get('/api/borrowing', (req, res) => {
+  connection.query('SELECT * FROM borrowing', (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error retrieving data from database');
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
 
 app.listen (port, (err=>{
     if (err){
